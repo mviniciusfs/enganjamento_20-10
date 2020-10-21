2bdf61a2bdcde83735ebea3bc5d17c74db6cbb54
Marcos_Vinici10 <viniciusfigueiredo@ucl.br>
2020-10-21 01:26:36 +0000

    NEW V FIN

--- Veiculo.cs
+++ Veiculo.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 
 class Veiculo
 {
-  public string ModeloCarro;
-  public double Consumo;
+  string ModeloCarro;
+  public string getModeloCarro()
+  {
+    return ModeloCarro;
+  }
+
+  double Consumo;
+  public double getConsumo()
+  {
+    return Consumo;
+  }
 
   public Veiculo(string mc, double c)
   {
     ModeloCarro = mc;
     Consumo = c;
   }
-
 
 }
--- main.cs
+++ main.cs
@@ -5,46 +5,57 @@ class MainClass
 {
   public static void Main (string[] args) 
   {
+    /*
     Veiculo u = new Veiculo("Uno", 13);
     Veiculo j = new Veiculo("Jetta", 8);
     Veiculo p = new Veiculo("Palio", 12);
     Veiculo f = new Veiculo("Fusca", 18);
-
+    
     List<string> carros = new List<string>();
     List<double> consumo = new List<double>();
-    
-    carros.Add(u.ModeloCarro);
-    consumo.Add(u.Consumo);
+    */
 
-    carros.Add(j.ModeloCarro);
-    consumo.Add(j.Consumo);
+    string continuar = "S", marca;
+    double consumo;
 
-    carros.Add(p.ModeloCarro);
-    consumo.Add(p.Consumo);
+    Veiculo v = new Veiculo("Teste", 0);
+    List<Veiculo> listaVeiculo = new List<Veiculo>();
 
-    carros.Add(f.ModeloCarro);
-    consumo.Add(f.Consumo);
-
-    string maisEconomico = carros[0];
-    double registroConsumo = consumo[0];
-
-    for( int i = 1; i < carros.Count ; i++ )
+    while(continuar == "S")
     {
-      if( consumo[i] > registroConsumo )
+      Console.Write("Marca do Carro: ");
+      marca = Console.ReadLine();
+      Console.Write("KM/L DO CARRO >>");
+      consumo = double.Parse(Console.ReadLine());
+      v = new Veiculo(marca, consumo);
+      listaVeiculo.Add(v);
+      Console.WriteLine("CONTINUAR? S/N");
+      continuar = Console.ReadLine();
+    }
+
+
+    Veiculo menor_consumo = listaVeiculo[0];
+
+    for(int i=1;i<listaVeiculo.Count;i++)
+    {
+      if(listaVeiculo[i].getConsumo() > menor_consumo.getConsumo())
       {
-        maisEconomico = carros[i];
-        registroConsumo = consumo[i];
+        menor_consumo = listaVeiculo[i];
       }
+      
     }
+    Console.WriteLine("MARCA DE MENOR CONSUMO: {0} \n CONSUMO DE: {1} KM/L", menor_consumo.getModeloCarro(), menor_consumo.getConsumo());
 
-    Console.WriteLine("Carro com maior autonomia: " + maisEconomico );
-    Console.WriteLine("Consumo registrado: " + registroConsumo );
-
-    for( int i = 0; i < carros.Count ; i++ )
-    {
-      double valorLitro = ( 1000 / consumo[i] ) * 4.89;
-      Console.WriteLine( "Carro: " + carros[i] );
-      Console.WriteLine( "Valor: " + valorLitro );
+    for(int i=0;i<listaVeiculo.Count;i++){
+      Console.WriteLine("-------------{0}------------", listaVeiculo[i].getModeloCarro());
+      Console.WriteLine("LITROS GASTOS EM 1000 KM: {0}", 1000/listaVeiculo[i].getConsumo());
+      Console.WriteLine("VALOR GASTO AO PERCORRER 1000 KM {0}", (1000/listaVeiculo[i].getConsumo())*4.89);    
     }
+    
+
+
+    
+
+   
   }
 }
